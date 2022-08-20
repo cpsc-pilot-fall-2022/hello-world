c6f576f905ca0deb80bf1aba248cf6d0b6e5c677
Kevin Wortman <kwortman@fullerton.edu>
2022-08-19 17:23:30 -0700

    implement hello world

---
+++ main.cc
@@ -0,0 +1,21 @@
+// Kevin Wortman
+// CPSC 120-01
+// 2022-08-19
+// kwortman@fullerton.edu
+// @kevinwortman
+//
+// Lab 01
+//
+// Hello world!
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
